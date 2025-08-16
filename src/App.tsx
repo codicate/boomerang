4c9012c1ebee87f4c15ab138118764f62ff264fa
Henry Liu <codicate@gmail.com>
2025-08-16 19:52:01 -0400

    installed dynamics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <DynamicWidget />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <DynamicContextProvider
+      settings={{
+        environmentId: import.meta.env.VITE_DYNAMIC_ENVIRONMENT_ID,
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </DynamicContextProvider>
+  </StrictMode>
+);
