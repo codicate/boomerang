cb993bad2896bac66ae1e709ae24b8d8024f91fc
Henry Liu <codicate@gmail.com>
2025-08-17 03:42:37 -0400

    replaced svg with lucide react

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -147,13 +147,6 @@ export const ResourceCard = ({
                   ? "fill-current"
                   : "stroke-current fill-none"
               }`}
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M5 15l7-7 7 7"
               />
             </svg>
             {resource.upvotes}
